e6421d57486f0f065adbdfd1cdc3352661699432
Pyro-Tech-Tech <57050111+Pyro-Tech-Tech@users.noreply.github.com>
2020-01-11 16:52:58 -0800

    Update HardwarePushbot.java

--- HardwarePushbot.java
+++ HardwarePushbot.java
@@ -1,107 +1,118 @@
-
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package FinalCode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.lang.Object;
 
-/**
- * This file illustrates the concept of driving a path based on time.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backwards for 1 Second
- *   - Stop and close the claw.
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+public class HardwarePushbot
+{
+    /* Public OpMode members. */
+    public DcMotor        leftDrive      = null;
+    public DcMotor        rightDrive     = null;
+    public DcMotor        auxLeftDrive   = null;
+    public DcMotor        auxRightDrive  = null;
+    public Servo          leftClaw       = null;
+    public Servo          rightClaw      = null;
+    public Servo          leftServoArm   = null;
+    public Servo          rightServoArm  = null;
+    public Servo          auxHand        = null;
+    public Servo          auxArm         = null;
+    public Servo          gripper        = null;
+    public DistanceSensor sensorRange    = null;
 
-@Autonomous(name="HardwarePushBot: Right Simple Auto", group="HardwarePushBot")
 
-public class Right_Autonomous_Simple extends LinearOpMode {
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
 
-    /* Declare OpMode members. */
-    HardwarePushbot         robot   = new HardwarePushbot();   // Use a Pushbot's hardware
-    private ElapsedTime     runtime = new ElapsedTime();
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
 
-   // static final double     FORWARD_SPEED = 0.6;
-   // static final double     TURN_SPEED    = 0.5;
+    /* Constructor */
+    public HardwarePushbot(){
 
-    @Override
-    public void runOpMode() {
+    }
 
-        /*
-         * Initialize the drive system variables.
-         * The init() method of the hardware class does all the work here
-         */
-        robot.init(hardwareMap);
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready to run");    //
-        telemetry.update();
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
         
-        robot.drive (1, 1, 1);
+        // Save reference to Hardware map
+        hwMap = ahwMap;
 
-        //Park, stop motors
-        robot.leftDrive.setPower(0);
-        robot.rightDrive.setPower(0);
-        robot.auxLeftDrive.setPower(0);
-        robot.auxRightDrive.setPower(0);
-        sleep(1000);
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
+        // Define and Initialize Motors
+        leftDrive     = hwMap.get(DcMotor.class, "frontLeft");
+        rightDrive    = hwMap.get(DcMotor.class, "frontRight");
+        auxLeftDrive  = hwMap.get(DcMotor.class, "backLeft");
+        auxRightDrive = hwMap.get(DcMotor.class, "backRight");
+        
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);     //Reverse direction for ease of use
+//        auxLeftDrive.setDirection(DcMotor.Direction.REVERSE);  //Reverse direction for ease of use
 
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        auxLeftDrive.setPower(0);
+        auxRightDrive.setPower(0);
+        
+        // Set all motors to run without encoders.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        auxLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        auxRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        telemetry.addData("Path", "Complete");
-        telemetry.addData("leftDrive", "%.2f", robot.leftDrive.getCurrentPosition());
-        telemetry.addData("rightDrive", "%.2f", robot.rightDrive.getCurrentPosition());
-        telemetry.addData("auxLeftDrive", "%.2f", robot.auxLeftDrive.getCurrentPosition());
-        telemetry.addData("auxRightDrive", "%.2f", robot.auxRightDrive.getCurrentPosition());
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
 
-        telemetry.update();
+        // Define and initialize ALL installed servos.
+        // leftClaw  = hwMap.get(Servo.class, "left_hand");
+        // rightClaw = hwMap.get(Servo.class, "right_hand");
+        // leftServoArm  = hwMap.get(Servo.class, "left_arm");
+        // rightServoArm = hwMap.get(Servo.class, "right_arm");
+        // auxHand  = hwMap.get(Servo.class, "aux_hand");
+        // auxArm = hwMap.get(Servo.class, "aux_arm");
+        // leftClaw.setPosition(MID_SERVO);
+        // rightClaw.setPosition(MID_SERVO);
+        // leftServoArm.setPosition(MID_SERVO);
+        // rightServoArm.setPosition(MID_SERVO);
+        // auxHand.setPosition(MID_SERVO);
+        // auxArm.setPosition(MID_SERVO);
+        //gripper  = hwMap.get(Servo.class, "1stHubPort5");
+
+        // you can use this as a regular DistanceSensor.
+        //sensorRange = hwMap.get(DistanceSensor.class, "sensor_range");
+
     }
     
-}
+    public void drive (double power, int dir, int time) {
+        leftDrive.setPower(dir*power);
+        auxLeftDrive.setPower(dir*power);
+        rightDrive.setPower(-dir*power);
+        auxRightDrive.setPower(-dir*power);
+        ElapsedTime timer = new ElapsedTime();
+        while (timer.seconds()<time) {
+        }
+        leftDrive.setPower(0);
+        auxLeftDrive.setPower(0);
+        rightDrive.setPower(0);
+        auxRightDrive.setPower(0);
+    }
+    
+    public void strafe (double power, int dir, int time) {
+        leftDrive.setPower(dir*power);
+        auxLeftDrive.setPower(dir*power);
+        rightDrive.setPower(-dir*power);
+        auxRightDrive.setPower(-dir*power);
+        ElapsedTime timer = new ElapsedTime();
+        while (timer.seconds()<time) {
+        }
+        leftDrive.setPower(0);
+        auxLeftDrive.setPower(0);
+        rightDrive.setPower(0);
+        auxRightDrive.setPower(0);
+    }
+    
+ }
